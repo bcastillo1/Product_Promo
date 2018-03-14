3b1594e027936d9932a4e4d5adefcdc6a24a2c90
BrittanyCastillo <bcastillo1@eagles.ewu.edu>
2018-03-13 19:48:02 -0700

    Code Entered

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// Plugin: https://codepen.io/manikoth/pen/RMbomv.js
+
+$('.img-holder').imageScroll({
+  parallax: true,
+  coverRatio: 0.5
+});
+
+$('.stop').imageScroll({
+  parallax: false,
+  coverRatio: 0.5
+});
+$('.gallery').lightGallery();
