88ba3c0f1ab9006ad40155d7657df7f8a672fdf3
BrittanyCastillo <bcastillo1@eagles.ewu.edu>
2018-03-22 22:33:22 -0700

    code

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
+console.log('Be like Browser.');
+
+// Light Gallery
+$('.gallery').lightGallery();
 // Plugin: https://codepen.io/manikoth/pen/RMbomv.js
 
 $('.img-holder').imageScroll({
   parallax: true,
   coverRatio: 0.5
 });
-
-$('.stop').imageScroll({
-  parallax: false,
-  coverRatio: 0.5
-});
-$('.gallery').lightGallery();
